01c4aa45d37e08f0380a396ca1befa7326029d72
lijianjun <574891945@qq.com>
2024-03-14 10:21:24 +0800

    add friebase

--- firebase.js
+++ firebase.js
@@ -1,15 +1,19 @@
 const { initializeApp } = require('firebase/app');
+const { initializeFirestore } = require( 'firebase/firestore')
 
 const firebaseConfig =  {
         apiKey: "AIzaSyASOhnQDR56cw3F8W8CCyigPiTPYZpB260",
-            authDomain: "advertising-85cfc.firebaseapp.com",
-            projectId: "advertising-85cfc",
-            storageBucket: "advertising-85cfc.appspot.com",
-            messagingSenderId: "669975544992",
-            appId: "1:669975544992:web:390a663b987ba059d2b5c5",
-    }
+        authDomain: "advertising-85cfc.firebaseapp.com",
+        projectId: "advertising-85cfc",
+        storageBucket: "advertising-85cfc.appspot.com",
+        messagingSenderId: "669975544992",
+        appId: "1:669975544992:web:390a663b987ba059d2b5c5",
+}
 
 
- const firebase = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const firebase = initializeFirestore(app, {
+    experimentalForceLongPolling: true,
+});
 
 module.exports = firebase
